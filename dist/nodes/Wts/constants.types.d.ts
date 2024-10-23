f00ed1c17c12dfc85eb7a8616ec91327320eb305
JoseHenrique975 <jose.henrique@helena.app>
2024-10-23 17:31:37 -0300

    Feat(n8n): First Commit

---
+++ dist/nodes/Wts/constants.types.d.ts
@@ -0,0 +1,4 @@
+export declare class Constants {
+    static baseUrl: string;
+}
+export declare const notSend = "NOT_SEND";
---
+++ dist/nodes/Wts/constants.types.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.notSend = exports.Constants = void 0;
+class Constants {
+}
+exports.Constants = Constants;
+Constants.baseUrl = 'https://api.wts.chat';
+exports.notSend = 'NOT_SEND';
+//# sourceMappingURL=constants.types.js.map
